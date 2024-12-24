ccc931a82882cfe1d617e015d90ef89d449edf23
Santhiya223 <sandysanthiya805@gmail.com>
2024-12-24 13:48:40 +0530

    changes in vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [require('tailwindcss'), require('autoprefixer')],
+    },
+  },
   server: {
     port : 5000,
     proxy: {"/api": {
